b674ccffea3d45083e9d0a21709d954304772100
Hudson <hdsonsilva@gmail.com>
2018-07-23 12:16:50 -0300

    Inicial

---
+++ www/js/login.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+
+      //Verificando se existem dados de usuario e senha salvos
+      if(localStorage.getItem('login_lembreme') == 1){
+
+        $('#input-lembreme').prop('checked','1');
+        $('#username').val(localStorage.getItem('login_username'));
+        $('#password').val(localStorage.getItem('login_password'));
+
+      }
+
+      // Fazendo login
+      $('#login').click( function(e) {
+
+
+        var username = $('#username').val();
+        var password = $('#password').val();
+            //Escondendo botao no inicio do processo de login
+            $('#button').hide();
+            $('#progress').show();
+            //Verificando o Lembre-se e armazenando os dados de login e senha
+            if($('#input-lembreme').prop('checked')){
+              console.log('marcado' );
+              localStorage.setItem('login_username', username);
+              localStorage.setItem('login_password', password);
+              localStorage.setItem('login_lembreme', 1);
+
+            }
+            else{
+              localStorage.setItem('login_lembreme', 0);
+            }
+
+            $.post(
+              'http://localhost/financeiro/sicof/usuarios/logintoken.php',
+              {
+                'username'  : username,
+                'password'  : password,
+                'action'    : 'LOGIN'
+              },
+              function(ret){
+                if(ret.token){
+                  ons.notification.alert('Login efetuado com sucesso.');
+                  localStorage.setItem('token',ret.token);
+
+                  window.location.href = './app.html';
+                }
+                else{
+                  ons.notification.alert(ret.erro);
+                }
+                //Exibindo o botao se o login der errado
+                $('#button').show();
+                $('#progress').hide();
+              },
+              'json'
+            );  
+      });
+
+    });

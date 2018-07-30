b70d6a33c37763bcfc4844616d245cea199b5a90
Hudson <hdsonsilva@gmail.com>
2018-07-30 18:39:34 -0300

    Incluindo busca de tarefas

---
+++ www/js/acoes.js
@@ -0,0 +1,34 @@
+function buscaAcoes(){
+	showModal('show');
+	$.ajax({
+              type: 'POST',
+              url : server_action,
+              data: {
+                'token'  : localStorage.getItem('token'),
+                'action' : 'LISTAR'
+              },
+              success:function(ret){
+                //Se retornar um token valido de acesso
+                
+                if(ret.dados){
+                  //ons.notification.alert(JSON.stringify(myApp.services));
+                  
+                  
+                  for ( i in ret.dados){
+                    myApp.services.tasks.create(ret.dados[i]);
+                  }
+                  
+                }
+                else{
+                  
+                  ret = null;
+                
+                }
+
+                showModal('hide');
+              
+              },
+              dataType:'json',
+              async:false
+          }); 
+}

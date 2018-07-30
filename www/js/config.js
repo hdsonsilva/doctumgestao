82a6d28c17a6d1eeed495f2b79d47a48681261b9
Hudson <hdsonsilva@gmail.com>
2018-07-30 18:40:45 -0300

    Incluindo busca de tarefas

--- www/js/config.js
+++ www/js/config.js
@@ -5,7 +5,7 @@ var server_auth = server + 'logintoken.php';
 var server_action = server + 'acoes.php';
 
 var idonesignal = "e259628e-f324-44a2-a029-09d47774ef6d";
-var debug = 0 ;
+var debug = 0 ; 
 
 localStorage.setItem('app_timeverify_auth', 30000);
 localStorage.setItem('server_auth', server_auth);
